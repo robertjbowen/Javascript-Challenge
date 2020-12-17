b69a2eff7d9eb536b11684f9357bb7aabe5e4cc0
robertjbowen <69124118+robertjbowen@users.noreply.github.com>
2020-12-17 16:49:41 -0600

    activate function using datetime and button click

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,24 +2,31 @@
 var tableData = data;
 
 // place holder to control date until html input and button figured out
-var datetime = "1/10/2010"
-var filterDate = datetime
+//var datetime = "1/10/2010"
+//var filterDate = datetime
 //var filterDate = document.getElementById('datetime').value;
 //console.log(filterDate);
 // YOUR CODE HERE!
-
+var UFOdate = []
 
 // Filter the UFO data from data.js by date 
 function UFOsighting (encounter) {
+	var filterDate = document.getElementById('datetime').value;
+	//console.log(filterDate);
 
 	if (encounter.datetime === filterDate) {
+		console.log(encounter);
 		sighted = true;
+		UFOdate.push(encounter);
+		//console.log(UFOdate)
 	}
 	else {
 		sighted = false;
 	}
 	return sighted;
+	//var UFOdate = UFOsightings;
+	//console.log(UFOdate);
 }
 
-var UFOdate = tableData.filter(UFOsighting);
-console.log(UFOdate);
+//var UFOdate = tableData.filter(UFOsighting);
+//console.log(UFOdate);
